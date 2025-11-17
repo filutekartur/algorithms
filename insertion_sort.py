dd1d742cf37485aec9f1357b6de1dfc6c3656efe
artur <filutekartur@interia.pl>
2025-11-17 20:09:48 +0100

    insertion sort finished

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,16 +1,20 @@
 from random import randint
 
-lista1=[randint(0,10000) for x in range(randint(1,100))]
-lista2=[3,2,1]
-lista=lista2
-
 def ins(l):
     i,j,tmp=1,1,0
-    en=len(l)-1
-    while i<=en:
+    while i<=(len(l)-1):
         j=i
         temp=l[j]
-        while temp<l[j-1]:
-            l[j]=l[j-1]
-            j-=1
+        while True:
+            if temp<l[j-1] and j>0:
+                l[j]=l[j-1]
+                j-=1
+            else:
+                l[j]=temp
+                break
         i+=1
+
+lista=[randint(0,1000) for x in range(randint(1,100))]
+ins(lista)
+if sorted(lista):
+     print(True)
